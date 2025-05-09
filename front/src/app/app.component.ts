91d79b974305fbf82d5ee022b00cf1569b4a2bba
Camila <camilaferna140494@gmail.com>
2025-05-09 17:36:14 -0300

    feat: create navbar

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,15 +1,34 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSidenavModule, MatListModule, MatIconModule],
+  imports: [
+    RouterOutlet,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'frontend';
+  constructor(private router: Router) {}
+
+  navigateToDashboard() {
+    this.router.navigate(['/dashboard']);
+  }
+
+  navigateToBooking() {
+    this.router.navigate(['/booking']);
+  }
+  ngOnInit() {
+    const currentUrl = window.location.href;
+    console.log(currentUrl);
+  }
 }
