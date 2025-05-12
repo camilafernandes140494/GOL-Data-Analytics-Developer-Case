946f27d782f7985adff2470847f70957e3b96c56
Camila <camilaferna140494@gmail.com>
2025-05-12 14:28:15 -0300

    WIP: working on screen layout

--- front/src/app/features/dashboard/dashboard.component.ts
+++ front/src/app/features/dashboard/dashboard.component.ts
@@ -1,11 +1,243 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { CommonModule } from '@angular/common';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import {
+  DashboardService,
+  DataItemChart,
+  DataItemTable,
+  TableDataResponse,
+} from './dashboard.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { formatDate } from '@angular/common';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
+registerLocaleData(localePt, 'pt-BR');
+
+interface ChartItem {
+  name: string;
+  value: number;
+}
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule, HeaderComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    NgxChartsModule,
+    MatCardModule,
+    MatSelectModule,
+    FormsModule,
+    MatGridListModule,
+
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+
+    ReactiveFormsModule,
+
+    MatProgressSpinnerModule,
+    MatSortModule,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  isLoadingChartData1: boolean = false;
+  monthSelectChartData1: string = '';
+  dataData1: ChartItem[] = [];
+
+  isLoadingChartData2: boolean = false;
+  monthSelectChartData2: string = '';
+  dataData2: ChartItem[] = [];
+
+  isLoadingChartData3: boolean = false;
+  dataData3: ChartItem[] = [];
+
+  isLoadingDashboardData: boolean = false;
+  dashboardDataItems: ChartItem[] = [];
+
+  dataSource = new MatTableDataSource<DataItemTable>();
+  displayedColumns: string[] = ['date', 'iatapair', 'departures', 'arrivals'];
+
+  constructor(
+    private dashboardService: DashboardService,
+    private snackBar: MatSnackBar
+  ) {}
+
+  months = [
+    { value: '01', name: 'Janeiro' },
+    { value: '02', name: 'Fevereiro' },
+    { value: '03', name: 'Março' },
+    { value: '04', name: 'Abril' },
+    { value: '05', name: 'Maio' },
+    { value: '06', name: 'Junho' },
+    { value: '07', name: 'Julho' },
+    { value: '08', name: 'Agosto' },
+    { value: '09', name: 'Setembro' },
+    { value: '10', name: 'Outubro' },
+    { value: '11', name: 'Novembro' },
+    { value: '12', name: 'Dezembro' },
+  ];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  totalCount = 0;
+
+  ngAfterViewInit() {
+    this.paginator.page.subscribe(() => {
+      this.dashboardData(this.paginator.pageIndex, this.paginator.pageSize);
+    });
+
+    this.dataSource.sort = this.sort;
+  }
+
+  ngOnInit() {
+    this.chartData1();
+    this.chartData2();
+    this.chartData3();
+    this.dashboardData();
+  }
+
+  formatXAxisLabel(value: string): string {
+    return formatDate(value, 'dd/MM', 'pt-BR');
+  }
+
+  chartData1() {
+    this.isLoadingChartData1 = true;
+
+    this.dashboardService.getChartData1({ limit: 5000 }).subscribe({
+      next: (response) => {
+        const allData = response.data.map((item) => ({
+          name: item.category,
+          value: item.value,
+        }));
+
+        if (this.monthSelectChartData1) {
+          this.dataData1 = allData.filter((item) => {
+            const month = new Date(item.name).getMonth() + 1; // getMonth() retorna 0-11
+            const monthString = month.toString().padStart(2, '0'); // sempre com 2 dígitos
+            return monthString === this.monthSelectChartData1;
+          });
+        } else {
+          this.dataData1 = allData;
+        }
+
+        this.isLoadingChartData1 = false;
+      },
+      error: () => {
+        this.snackBar.open('Erro ao carregar os gráficos.', '', {
+          duration: 3000,
+          horizontalPosition: 'right',
+          verticalPosition: 'top',
+          panelClass: ['snackbar-error'],
+        });
+        this.isLoadingChartData1 = false;
+      },
+    });
+  }
+
+  chartData2() {
+    this.isLoadingChartData2 = true;
+
+    this.dashboardService.getChartData2({ limit: 5000 }).subscribe({
+      next: (response) => {
+        const allData = response.data.map((item) => ({
+          name: item.category,
+          value: item.value,
+        }));
+
+        if (this.monthSelectChartData2) {
+          this.dataData2 = allData.filter((item) => {
+            const month = new Date(item.name).getMonth() + 1; // getMonth() retorna 0-11
+            const monthString = month.toString().padStart(2, '0'); // sempre com 2 dígitos
+            return monthString === this.monthSelectChartData2;
+          });
+        } else {
+          this.dataData2 = allData;
+        }
+
+        this.isLoadingChartData2 = false;
+      },
+      error: () => {
+        this.snackBar.open('Erro ao carregar os gráficos.', '', {
+          duration: 3000,
+          horizontalPosition: 'right',
+          verticalPosition: 'top',
+          panelClass: ['snackbar-error'],
+        });
+        this.isLoadingChartData2 = false;
+      },
+    });
+  }
+
+  chartData3() {
+    this.isLoadingChartData3 = true;
+
+    this.dashboardService.getChartData3({ limit: 5000 }).subscribe({
+      next: (response) => {
+        const allData = response.data.map((item) => ({
+          name: item.category,
+          value: item.value,
+        }));
+
+        this.dataData3 = allData;
+
+        this.isLoadingChartData3 = false;
+      },
+      error: () => {
+        this.snackBar.open('Erro ao carregar os gráficos.', '', {
+          duration: 3000,
+          horizontalPosition: 'right',
+          verticalPosition: 'top',
+          panelClass: ['snackbar-error'],
+        });
+        this.isLoadingChartData3 = false;
+      },
+    });
+  }
+
+  dashboardData(pageIndex: number = 0, pageSize: number = 5) {
+    this.isLoadingDashboardData = true;
+
+    this.dashboardService
+      .getDashboardData({
+        limit: pageSize * (pageIndex + 1),
+      })
+      .subscribe({
+        next: (response: TableDataResponse) => {
+          console.log(response);
+          this.dataSource.data = response.data.slice(-pageSize);
+          this.totalCount = response.count;
+          this.isLoadingDashboardData = false;
+        },
+        error: () => {
+          this.snackBar.open('Erro ao carregar dados da tabela.', '', {
+            duration: 3000,
+            horizontalPosition: 'right',
+            verticalPosition: 'top',
+            panelClass: ['snackbar-error'],
+          });
+          this.isLoadingDashboardData = false;
+        },
+      });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+}
--- front/src/app/features/dashboard/dashboard.service.ts
+++ front/src/app/features/dashboard/dashboard.service.ts
@@ -1,25 +1,54 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from '../../services/api.service';
+export interface DataItemChart {
+  category: string;
+  value: number;
+}
 
+export interface ChartDataResponse {
+  count: number;
+  limit: number;
+  data: DataItemChart[];
+}
+
+export interface DataItemTable {
+  date: string;
+  iatapair: string;
+  departures: number;
+  arrivals: number;
+}
+export interface TableDataResponse {
+  count: number;
+  limit: number;
+  data: DataItemTable[];
+}
 @Injectable({
   providedIn: 'root',
 })
 export class DashboardService {
   constructor(private apiService: ApiService) {}
 
-  getChartData1() {
-    return this.apiService.get('/dashboard/chart/data/1');
+  getChartData1(params: { limit?: number }) {
+    return this.apiService.get<ChartDataResponse>(
+      `/api/v1/dashboard/chart/data/1?limit=${params.limit}`
+    );
   }
 
-  getChartData2() {
-    return this.apiService.get('/dashboard/chart/data/2');
+  getChartData2(params: { limit?: number }) {
+    return this.apiService.get<ChartDataResponse>(
+      `/api/v1/dashboard/chart/data/2?limit=${params.limit}`
+    );
   }
 
-  getChartData3() {
-    return this.apiService.get('/dashboard/chart/data/3');
+  getChartData3(params: { limit?: number }) {
+    return this.apiService.get<ChartDataResponse>(
+      `/api/v1/dashboard/chart/data/3?limit=${params.limit}`
+    );
   }
 
-  getDashboardData() {
-    return this.apiService.get('/dashboard/data');
+  getDashboardData(params: { limit?: number }) {
+    return this.apiService.get<TableDataResponse>(
+      `/api/v1/dashboard/data?limit=${params.limit}`
+    );
   }
 }
